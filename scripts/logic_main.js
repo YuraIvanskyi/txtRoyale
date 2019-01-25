6accebb2db5ac4a96f808a352cf1c420d37b1733
Yura Ivanskyi <yuraivanskyi@gmail.com>
2019-01-25 17:26:08 +0200

    Added controls, adjusted style. Extended stats logic.

--- scripts/logic_main.js
+++ scripts/logic_main.js
@@ -9,31 +9,16 @@ window.onload = function (){
     //this.myChart4 = createChart(ctx4);
 }
 
-function genreateValues() {
-    return [parseInt(Math.random()*100),parseInt(Math.random()*100),parseInt(Math.random()*100),parseInt(Math.random()*100)]
+function generateValues() {
+    var values = [parseInt(Math.random()*100),parseInt(Math.random()*100),parseInt(Math.random()*100),parseInt(Math.random()*100),parseInt(Math.random()*100)];
+    return values;
 }
 function createChart(canvasObj) {
     return new Chart(canvasObj, {
         type: 'radar',
         data: {
-            labels: ["Attack", "HP", "Resist", "Regeneration"],
-            datasets: [{
-                label: '# of Votes',
-                data: genreateValues(),
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.7)',
-                    'rgba(54, 162, 235, 0.7)',
-                    'rgba(255, 206, 86, 0.7)',
-                    'rgba(75, 192, 192, 0.7)'
-                ],
-                borderColor: [
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)'
-                ],
-                borderWidth: 1
-            }]
+            labels: ["Attack", "HP", "Defence", "Resist", "Regeneration"],
+            datasets: []
         },
         options: {
             scales: {
@@ -42,18 +27,33 @@ function createChart(canvasObj) {
                         beginAtZero:true
                     }
                 }]
-            }
+            },
+            title: {
+                display: true,
+                text: 'Battlefield'
+            },
         }
     });
 }
 function addFighter() {
-    ChampionsChart.options.title.text = 'Baka-charuto!';
-    ChampionsChart.options.display = true;
-    addData(ChampionsChart,'test',Math.random()*100);
-    ChampionsChart.update()
+    var newColor = generateRandomColor();
+    var championDataset = {
+        label: document.getElementsByName("NameField").innerText,
+        borderColor: newColor.color,
+        backgroundColor: newColor.alpha,
+        pointBorderColor: newColor.color,
+        data: generateValues(),
+    };
+
+    ChampionsChart.data.datasets.push(championDataset);
+    ChampionsChart.update();
 }
 function generateRandomColor () {
-    return `rgba(${parseInt(Math.random()*255)}, ${parseInt(Math.random()*255)}, ${parseInt(Math.random()*255)}, 0.7)`
+    var color = `${parseInt(Math.random()*255)}, ${parseInt(Math.random()*255)}, ${parseInt(Math.random()*255)}`
+    return {
+        alpha:`rgba(${color}, 0.2)`,
+        color:`rgba(${color}, 1)`
+    }
 }
 function addData(chart, label, data) {
     chart.data.labels.push(label);
@@ -63,4 +63,24 @@ function addData(chart, label, data) {
     chart.data.datasets[0].backgroundColor.push(generateRandomColor());
     chart.data.datasets[0].borderColor.push('rgba(0, 0, 0, 1)');
     chart.update();
+}
+function updateAttackFieldInput(val) {
+    var current = document.getElementById('AttackFieldInput').innerHTML
+    document.getElementById('AttackFieldInput').innerHTML = current.replace(/([\[(])(.+?)([\])])/g, `[${val}]`); 
+}
+function updateHPFieldInput(val) {
+    var current = document.getElementById('HPFieldInput').innerHTML
+    document.getElementById('HPFieldInput').innerHTML = current.replace(/([\[(])(.+?)([\])])/g, `[${val}]`); 
+}
+function updateResistFieldInput(val) {
+    var current = document.getElementById('ResistFieldInput').innerHTML
+    document.getElementById('ResistFieldInput').innerHTML = current.replace(/([\[(])(.+?)([\])])/g, `[${val}]`); 
+}
+function updateRegenFieldInput(val) {
+    var current = document.getElementById('RegenFieldInput').innerHTML
+    document.getElementById('RegenFieldInput').innerHTML = current.replace(/([\[(])(.+?)([\])])/g, `[${val}]`); 
+}
+function updateDefenceFieldInput(val) {
+    var current = document.getElementById('DefenceFieldInput').innerHTML
+    document.getElementById('DefenceFieldInput').innerHTML = current.replace(/([\[(])(.+?)([\])])/g, `[${val}]`); 
 }
